ba5d8ef3581f7d770b0ad37a08bfafa50da7157e
Dave Vaughan <david.vaughan@devopsgroup.com>
2019-08-30 10:20:23 +0100

    Trying something

--- manifests/elasticsearch.pp
+++ manifests/elasticsearch.pp
@@ -15,7 +15,7 @@ class profiles::elasticsearch {
   elasticsearch::instance { $::hostname:
     config      => {
       'network.host'                 => '0.0.0.0',
-      'cluster.initial_master_nodes' => $::hostname
+      'cluster.initial_master_nodes' => "${::hostname}-${::hostname}"
     },
     jvm_options => [
       '#PrintGCDetails',
