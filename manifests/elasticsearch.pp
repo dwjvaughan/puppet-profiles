a360e22e98af07661e3a8866477f56d625626fc4
Dave Vaughan <david.vaughan@devopsgroup.com>
2019-08-30 08:52:14 +0100

    Adding port

--- manifests/elasticsearch.pp
+++ manifests/elasticsearch.pp
@@ -3,4 +3,12 @@
 class profiles::elasticsearch {
   include elastic_stack::repo
   include elasticsearch
+  include ::filewalld
+
+  firewalld_port { 'Open Elasticsearch port':
+    ensure   => present,
+    zone     => 'public',
+    port     => 9200,
+    protocol => 'tcp',
+  }
 }
