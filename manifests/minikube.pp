aa698f28728b684b0a32ae86e9c4fa5f6016673a
Dave Vaughan <david.vaughan@devopsgroup.com>
2019-09-16 11:44:09 +0100

    Minikube

---
+++ manifests/minikube.pp
@@ -0,0 +1,28 @@
+# Minikube
+
+class profiles::minikube {
+  include firewalld
+
+  yumrepo { 'kubernetes':
+    ensure   => 'present',
+    name     => 'Kubernetes',
+    descr    => 'Kubernetes El 7 - $basearch',
+    baseurl  => 'https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64',
+    gpgkey   => 'https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg',
+    enabled  => '1',
+    gpgcheck => '1',
+    target   => '/etc/yum.repo.d/kubernetes.repo',
+  }
+
+  package { 'kubectl':
+    ensure  => 'installed',
+    require => Yumrepo['kubernetes'],
+  }
+
+  # firewalld_port { 'Open Logstash port':
+  #   ensure   => present,
+  #   zone     => 'public',
+  #   port     => 5044,
+  #   protocol => 'tcp',
+  # }
+}
